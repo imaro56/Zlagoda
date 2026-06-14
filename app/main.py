e1dfc792c50c45c1e3d43feba90b1236537e7c8c
Daria <deriidariaa@gmail.com>
2026-06-14 20:14:49 +0300

    Add check templates (check_detail, my_checks)

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from starlette.middleware.sessions import SessionMiddleware
 from app.config import settings
 from app.dependencies import RequiresAuth
 from starlette.responses import RedirectResponse
-from app.routers import auth, category, employee
+from app.routers import auth, category, employee, customer_card, product, store_product, check
 
 
 @asynccontextmanager
@@ -25,6 +25,12 @@ app.include_router(auth.router)
 app.include_router(category.router)
 app.include_router(employee.router)
 
+app.include_router(customer_card.router)
+app.include_router(product.router)
+app.include_router(store_product.router)
+app.include_router(check.router)
+
 @app.exception_handler(RequiresAuth)
 def requires_auth_handler(request: Request, exc: RequiresAuth):
     return RedirectResponse(url="/login", status_code=303)
+
